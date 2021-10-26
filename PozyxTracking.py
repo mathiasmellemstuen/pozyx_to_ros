68a5a47e465bc273a0831412c7688c3d66e7fb3d
Mathias <mathias@Mathiass-MacBook-Pro.local>
2021-10-27 00:38:18 +0200

    Fixed typos.

--- PozyxTracking.py
+++ PozyxTracking.py
@@ -3,10 +3,10 @@ from pypozyx import *
 
 class PozyxTracking:
     def __init__(self, port=None, remoteID=0x00, remote=False,
-                 anchers=List, algorithm=POZYX_POS_ALG_UWB_ONLY, dimention=POZYX_3D, height=1000):
+                 anchors=List, algorithm=POZYX_POS_ALG_UWB_ONLY, dimention=POZYX_3D, height=1000):
         self.deviceID   # ID of the master device
         self.networkID  # IF of the network
-        self.anchers = anchers
+        self.anchors = anchors
         self.position   # Positon of the tag
 
         if not remote:
@@ -17,7 +17,7 @@ class PozyxTracking:
         else:
             self.p = self.connectToTag(tagName=port)
 
-        self.setAnchoersManualy()
+        self.setAnchorsManually()
 
 
     def connectToTag(self, tagName=None):
@@ -29,13 +29,13 @@ class PozyxTracking:
         
         return PozyxSerial(serialPort)
 
-    def setAnchoersManualy(self):
+    def setAnchorsManually(self):
         status = self.p.clearDevices(self.remoteID)
 
-        for anchoer in self.anchers:
-            status &= self.p.addDevice(anchoer, self.remoteID)
+        for anchor in self.anchors:
+            status &= self.p.addDevice(anchor, self.remoteID)
         
-        if len(self.anchers) > 4:
+        if len(self.anchors) > 4:
             status &= self.p.setSelectionOfAnchors(POZYX_ANCHOR_SEL_AUTO)
 
         return status
@@ -44,6 +44,5 @@ class PozyxTracking:
         self.position = Coordinates()
         status = self.p.doPositioning(self.position, self.height, self.algorithm, remote_id=self.remoteID)
 
-    def recalibrateCoordinate(self, offsettX, offsettY, offsetZ):
-        """Recalibrate cordinates for the pozyx system"""
-
+    def recalibrateCoordinate(self, offsetX, offsetY, offsetZ):
+        """Recalibrate coordinates for the pozyx system"""

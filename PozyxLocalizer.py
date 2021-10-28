bcbc6ef5061c6f70b5bbb7b20f7c61dcdb99a96d
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2021-10-28 21:32:02 +0200

    Added docstrings and formatted code.

--- PozyxLocalizer.py
+++ PozyxLocalizer.py
@@ -1,6 +1,5 @@
 from typing import List
 from pypozyx import *
-from pythonosc.udp_client import SimpleUDPClient
 
 class XYZ:
     def __init__(self):
@@ -21,88 +20,87 @@ class XYZ:
 
 
 class PozyxLocalizer:
-    def __init__(self, port=None, remoteID=0x00, remote=False, useProcessing=True,
-                 anchors=List, algorithm=POZYX_POS_ALG_UWB_ONLY, dimension=POZYX_2D, height=1000,
-                 ip='127.0.01', networkPort=8888, oscUdpClient=None):
-        self.deviceID = None        # ID of the master device
-        self.networkID = None       # ID of the network
-        self.remoteID = remoteID
+    """
+    Pozyx localizer class
 
+    This class connects to a pozyx tag through a USB serial connection and returns the current position from the pozyx
+    environment. 
+
+    """
+    def __init__(self, port=None, remoteID=0x00, remote=False, anchors=List, algorithm=POZYX_POS_ALG_UWB_ONLY, dimension=POZYX_2D, height=1000):
+
+        self.deviceID = None # ID of the master device
+        self.position = None # Positon of the tag
+        self.pozyx = None
+        self.remoteID = remoteID
         self.algorithm = algorithm
         self.dimension = dimension
-        self.oscUdpClient = oscUdpClient
-
         self.anchors = anchors
-
         self.height = height
-        self.position = None   # Positon of the tag
-
-        # self.p = PozyxSerial(port)
-
+        
         if not remote:
             self.remoteID = None
 
         if port is None:
-            self.p = self.connectToTag()
+            self.pozyx = self.createSerialConnectionToTag()
         else:
-            self.p = self.connectToTag(tagName=port)
-
-        if useProcessing:
-            oscUdpClient = SimpleUDPClient(ip, networkPort)
+            self.pozyx = self.createSerialConnectionToTag(tagName=port)
 
         self.setAnchorsManually()
 
 
-    def connectToTag(self, tagName=None):
+    def createSerialConnectionToTag(self, tagName=None):
+        """
+        Creating a serial connection either automatically or manually with providing the tagName. Returns a PozyxSerial object.
+        """
+
         if tagName is None:
-            serialPort = get_serial_ports()[0].device
-            print(serialPort)
+            serialPort = get_first_pozyx_serial_port()
+            print(f'Auto assigning serial port: {serialPort}')
         else:
             serialPort = tagName
         
-        p = PozyxSerial(serialPort)
+        connection = PozyxSerial(serialPort)
 
-        if p is None:
+        if connection is None:
             print('No Pozyx connected. Check if one is connected')
 
-        return p
+        return connection
 
     def setAnchorsManually(self, saveToFlash=False):
-        status = self.p.clearDevices(self.remoteID)
+        """
+        Using anchors that is provided in self.anchors. 
+        """
+
+        status = self.pozyx.clearDevices(self.remoteID)
 
         for anchor in self.anchors:
-            status &= self.p.addDevice(anchor, self.remoteID)
+            status &= self.pozyx.addDevice(anchor, self.remoteID)
         
         if len(self.anchors) > 4:
-            status &= self.p.setSelectionOfAnchors(POZYX_ANCHOR_SEL_AUTO)
+            status &= self.pozyx.setSelectionOfAnchors(POZYX_ANCHOR_SEL_AUTO)
 
         if saveToFlash:
-            self.p.saveAnchorIds(remote_id=self.remoteID)
-            self.p.saveRegisters([PozyxRegisters.POSITIONING_NUMBER_OF_ANCHORS], remote_id=self.remoteID)
+            self.pozyx.saveAnchorIds(remote_id=self.remoteID)
+            self.pozyx.saveRegisters([PozyxRegisters.POSITIONING_NUMBER_OF_ANCHORS], remote_id=self.remoteID)
 
         return status
         
     def loop(self):
+        """
+        Getting the position of the connected tag and returning it. 
+        """
         self.position = Coordinates()
-        status = self.p.doPositioning(self.position, self.dimension, self.height, self.algorithm, remote_id=self.remoteID)
+        status = self.pozyx.doPositioning(self.position, self.dimension, self.height, self.algorithm, remote_id=self.remoteID)
 
         if status == POZYX_SUCCESS: 
             print(self.positionToString())
         else: 
             print(f'Error: Do positioning failed due to {"failure" if status == POZYX_FAILURE else "timeout"}.')
-        
-    def getOrigoDevice(self):
-        for anchor in self.anchors:
-            if anchor.pos.data[0] == 0 and anchor.pos.data[1] == 0:
-                return anchor
-
-        return None
 
     def recalibrateCoordinate(self, offsetX, offsetY, offsetZ):
         """Recalibrate coordinates for the pozyx system, to be removed"""
 
-        origoDevice = self.getOrigoDevice()
-        origo = origoDevice.pos.data
         newOrigo = XYZ()
 
         # Calculat the new origo for pozyx
@@ -138,6 +136,8 @@ class PozyxLocalizer:
                 anchor.pos.z = anchor.pos.z - offsetZ
     
     def positionToString(self):
+        """Returning a string with the x,y,z coordinates."""
+
         return f'Current position:\nX: {self.position[0] / 1000}\nY: {self.position[1] / 1000}\nZ: {self.position[2] / 1000}'
 
 
@@ -149,7 +149,7 @@ if __name__ == '__main__':
         DeviceCoordinates(0x6851, 1, Coordinates(176, -3328, 0))
     ]
 
-    localizer = PozyxLocalizer(anchors = anchors, port = "/dev/cu.usbmodem3551385E34381", remoteID=0x6e66)
+    localizer = PozyxLocalizer(anchors = anchors)
 
     while True:
         localizer.loop()

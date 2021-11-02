649da524b4c216ee4a7a481ed6126c24f1f91c9e
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2021-11-02 10:39:21 +0100

    Added comment on pozyxToRos function

--- scripts/PozyxToRos.py
+++ scripts/PozyxToRos.py
@@ -3,7 +3,10 @@ from PozyxLocalizer import PozyxLocalizer
 from geometry_msgs.msg import Vector3
 import rospy
 
-def pozyxToRos(): 
+def pozyxToRos():
+    """
+    Running a ROS publisher that publishes the position from pozyx as a Vector3.
+    """
     localizer = PozyxLocalizer(anchors = "/home/pi/pozyx_ws/src/pozyx_to_ros/config/PozyxConfig.yaml")
     pub = rospy.Publisher("pozyx", Vector3, queue_size = 10)
     rospy.init_node("PozyxToRos", anonymous = True)
